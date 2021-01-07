ae6aada2637847fc5790c5f2bf40f203f21e7e84
Florian Weißmeier <florian.weissmeier@gmx.de>
2021-01-07 23:53:08 +0100

    Added commands + bug fixes

--- client.py
+++ client.py
@@ -4,26 +4,40 @@ import sys
 import time
 
 class Client(threading.Thread):
-    def __init__(self, nick, host, port):
+    def __init__(self, chatApp):
         super(Client, self).__init__()
-        self.host = host
-        self.port = int(port)
-        self.nick = nick
+        self.chatApp = chatApp
+        self.isConnected = False
 
     def run(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn()
-        print('Verbunden!\n')
+        self.socket.settimeout(5)
 
-    def conn(self):
-        self.socket.connect((self.host, self.port))
-        self.socket.send(self.nick.encode())
-        
+    def conn(self, args):
+        if self.chatApp.nickname == "":
+            self.chatApp.sysMsg("You nickname is not set. Try /help to find out how.")
+            return False
+        host = args[0]
+        port = int(args[1])
+        self.chatApp.sysMsg("Connecting to {0} on port {1}".format(host, port))
+        try:
+            self.socket.connect((host, port))
+        except socket.error:
+            self.chatApp.sysMsg("Could not connect. Attempt timed out.")
+            return False
+        self.socket.send("%/nick {0}".format(self.chatApp.nickname).encode())
+        self.chatApp.sysMsg("Connected.")
+        self.isConnected = True
+    
+    def disconn(self):
+        self.socket.close()
+        self.isConnected = False
+        self.socket = None
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send(self, msg):
-        if msg == '':
-            return False
-        self.socket.send(msg.encode())
+        if msg != '':
+            self.socket.send(msg.encode())
 
 
     
--- server.py
+++ server.py
@@ -8,38 +8,79 @@ class Server(threading.Thread):
     def __init__(self, chatApp):
         super(Server, self).__init__()
         self.chatApp = chatApp
+        self.commandDict = {
+            "nick": [self.setPartnerNickname, 1],
+            "quit": [self.partnerQuit, 0],
+            "syntaxErr": [self.chatClientVersionsOutOfSync, 0]
+        }
+
+    def commandHandler(self, command):
+        command = command.decode().split(" ")
+        if len(command) > 1:
+            args = command[1:]
+        command = command[0][2:]
+        if not command in self.commandDict:
+            self.chatApp.sysMsg("Your partner sent an invalid command. Make sure they use the same version as you!")
+            self.chatApp.chatClient.send("%/syntaxErr")
+        else:
+            if self.commandDict[command][1] == 0:
+                self.commandDict[command][0]()
+            elif len(args) == self.commandDict[command][1]:
+                self.commandDict[command][0](args)
+            else:
+                self.chatApp.sysMsg("Your partner sent an invalid command syntax.")
+                self.chatApp.sysMsg("Make sure they use the same version as you!")
+                self.chatApp.chatClient.send("%/syntaxErr")
 
     def run(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((HOST, PORT))
         self.socket.listen()
-        print("Server running on port {0}".format(PORT))
-        self.chatApp.chatForm.chatFeed.values.append("[SYSTEM] Server wurde auf Port {0} gestartet.".format(PORT))
+
+        self.chatApp.sysMsg("Started server on port {0}.".format(PORT))
+
         conn, addr = self.socket.accept()
-        nick = conn.recv(1024)
-        if not nick:
-            self.partner = "Unknown"
+        init = conn.recv(1024)
+        if not init:
+            self.chatApp.partner = "Unknown"
         else:
-            self.partner = str(nick.decode())
-        print("\n{0} ist beigetreten".format(self.partner))
-        self.chatApp.chatForm.chatFeed.values.append("[SYSTEM] {0} ist beigetreten.".format(self.partner))
-        self.chatApp.chatForm.chatFeed.display()
+            init = init.decode()
+            if init.startswith("%/"):
+                command = init[2:].split(' ')
+                self.chatApp.partner = command[1]
+            else:
+                self.chatApp.partner = "Unknown"
+
+        self.chatApp.sysMsg("{0} joined the chat.".format(self.chatApp.partner))
+        
         while True:
             if len(self.chatApp.chatForm.chatFeed.values) > 20:
                 self.chatApp.chatForm.chatFeed.values = []
+
             data = conn.recv(1024)
             if not data:
-                print("\nERROR: Empty message recieved")
-            if data.decode() == "/test":
-                continue
-            if data.decode() == "/quit":
-                print("[SYSTEM] {0} hat die Verbindung getrennt!\n Beende den Chat...".format(self.partner))
-                self.chatApp.chatForm.chatFeed.values.append("{0} hat die Verbindung getrennt!\n Zum beenden drücke STRG + Q.".format(self.partner))
-                self.chatApp.chatForm.chatFeed.display()
-                while True:
-                    pass
-            else:
-                print('\n{0}: {1}'.format(self.partner, data.decode()))
-                self.chatApp.chatForm.chatFeed.values.append("{0}>  {1}".format(self.partner, data.decode()))
-            self.chatApp.chatForm.chatFeed.display()
+                self.chatApp.sysMsg("ERROR: Recieved an empty message.")
 
+            if data.decode().startswith('%/'):
+                self.commandHandler(data)
+            else:
+                self.chatApp.chatForm.chatFeed.values.append("{0}>  {1}".format(self.chatApp.partner, data.decode()))
+                self.chatApp.chatForm.chatFeed.display()
+
+    def disconn(self):
+        self.socket.close()
+        self.socket = None
+        self.run()
+
+    def setPartnerNickname(self, nick):
+        oldNick = self.chatApp.partner
+        self.chatApp.partner = nick[0]
+        self.chatApp.sysMsg("{0} changed their name to {1}".format(oldNick, nick[0]))
+
+    def partnerQuit(self):
+        self.chatApp.sysMsg("{0} left the chat. Disconnecting services...".format(self.chatApp.partner))
+        self.chatApp.disconn()
+
+    def chatClientVersionsOutOfSync(self):
+        self.chatApp.sysMsg("An error occured while communicating to your partners system. Please make sure that you use the same version as them.")
+        
